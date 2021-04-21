d0cdd8a10c4c787ee71ce29068b907a8d7f75f70
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-04-21 12:10:00 +0430

    mobile menu navigation added

--- src/layout/mobileMenu/mobileMenuOverlay/mobileMenuOverlay.component.jsx
+++ src/layout/mobileMenu/mobileMenuOverlay/mobileMenuOverlay.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './mobileMenuOverlay.scss';
+import { Link } from 'gatsby';
+import * as classes from './mobileMenuOverlay.module.scss';
 
 const MobileMenuOverlay = ({ closeOverlay }) => {
     // const [ clickedState, setClickedState ] = useState(false)
@@ -10,7 +11,7 @@ const MobileMenuOverlay = ({ closeOverlay }) => {
 
     return (
         <div 
-            className={ `overlay-mobileMenu` }
+            className={ classes.overlay }
             onClick={ closeOverlayFn }
             onKeyDown={ e => console.log(e) }
             id="mobileMenuOverlay"
@@ -18,8 +19,29 @@ const MobileMenuOverlay = ({ closeOverlay }) => {
             aria-label="button"
             tabIndex="0"
             >
-            <div className={ 'inner' }>
-
+            <div className={ classes.inner }>
+                <ul className={ classes.ul }>
+                    <li className={ classes.li }>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className={ classes.li }>
+                        <Link to="/projects">
+                            Projects
+                        </Link>
+                    </li>
+                    <li className={ classes.li }>
+                        <Link to="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li className={ classes.li }>
+                        <Link to="/contact">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </div>
     );
